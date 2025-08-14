73bc21bcaa3bea212c1aa2387439114e311bf48b
mdhanifroni95 <eng.hanif.roni63@gmail.com>
2025-08-14 21:49:34 +0600

    feat: implement authentication module with local strategy and user validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PropertyModule } from "./property/property.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 import dbConfig from "./config/db.config";
 import dbConfigProduction from "./config/db.config.production";
 
@@ -17,7 +18,8 @@ import dbConfigProduction from "./config/db.config.production";
     }),
     PropertyModule,
     TypeOrmModule.forRootAsync({ useFactory: process.env.NODE_ENV === "production" ? dbConfigProduction : dbConfig }),
-    UserModule],
+    UserModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
